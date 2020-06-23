660bac76898c1575b345fba1754abaa06feced0c
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-06-23 10:24:22 -0400

    initial commit

---
+++ OperatorOfExcercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OperatorOfExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
